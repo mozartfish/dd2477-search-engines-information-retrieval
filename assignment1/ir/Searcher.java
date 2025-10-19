ee92d23b59c7fa79d2355fb402b5b770e5d7121e
Pranav Rajan <rajan.pranav320@gmail.com>
2025-10-19 21:27:46 +0200

    Finish Task 1.4 - Multi-Word Queries

--- assignment1/ir/Searcher.java
+++ assignment1/ir/Searcher.java
@@ -7,6 +7,8 @@
 
 package ir;
 
+import java.util.ArrayList;
+
 /** Searches an index for results of a query. */
 public class Searcher {
 
@@ -41,9 +43,79 @@ public class Searcher {
         return oneWordQuery(query);
       } else {
         System.out.println("MULTI-WORD QUERY");
+        return multiWordQuery(query);
       }
     }
+    // phrase query
+    if (queryType == QueryType.PHRASE_QUERY) {
+      System.out.println("PHRASE QUERY");
+    }
     return null;
+  }
+
+  private PostingsList multiWordQuery(Query query) {
+    ArrayList<PostingsList> postingsLists = getPostingsList(query);
+
+    // size(postingsLists) == 0
+    if (postingsLists.isEmpty()) {
+      return new PostingsList();
+    }
+
+    // size(postingsLists) == 2
+    PostingsList p1 = postingsLists.getFirst();
+    PostingsList p2 = postingsLists.get(1);
+    PostingsList result = intersect(p1, p2);
+
+    // size(postingsLists) > 2
+    for (int i = 2; i < postingsLists.size(); i++) {
+      PostingsList postings = postingsLists.get(i);
+      if (postings != null) {
+        result = intersect(result, postings);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Compute the intersection of two postings lists
+   *
+   * @param p1 first postings list
+   * @param p2 second postings list
+   * @return postings list that contains the intersection of p1 and p2 postings list
+   */
+  private PostingsList intersect(PostingsList p1, PostingsList p2) {
+    PostingsList result = new PostingsList();
+    int i = 0;
+    int j = 0;
+    while (i < p1.size() && j < p2.size()) {
+      if (p1.get(i).docID == p2.get(j).docID) {
+        if (result.size() == 0 || result.get(result.size() - 1).docID != p1.get(i).docID) {
+          PostingsEntry postingEntry = new PostingsEntry(p1.get(i).docID, 1, 1);
+          result.add(postingEntry);
+        }
+        i++;
+        j++;
+      } else if (p1.get(i).docID < p2.get(j).docID) {
+        i++;
+      } else {
+        j++;
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Retrieve the postings lists for each term in the query
+   *
+   * @param query information requested by the user
+   * @return a list of postings associated with each term in the query
+   */
+  private ArrayList<PostingsList> getPostingsList(Query query) {
+    ArrayList<PostingsList> postingsList = new ArrayList<>();
+    for (int i = 0; i < query.size(); i++) {
+      postingsList.add(index.getPostings(query.queryterm.get(i).term));
+    }
+    return postingsList;
   }
 
   /**

22af9dea57e49621f54459a14d7e1ccbe3061186
Pranav Rajan <rajan.pranav320@gmail.com>
2025-10-19 22:50:00 +0200

    Finish Task 1.7 - Inverted Index as a hash table on Disk

--- assignment1/ir/PersistentHashedIndex.java
+++ assignment1/ir/PersistentHashedIndex.java
@@ -41,7 +41,8 @@ public class PersistentHashedIndex implements Index {
   public static final String DOCINFO_FNAME = "docInfo";
 
   /** The dictionary hash table on disk can fit this many entries. */
-  public static final long TABLESIZE = 15 * 611953L;
+//  public static final long TABLESIZE = 15 * 611953L;
+  public static final long TABLESIZE = 6 * 611953L;
 
   /** The dictionary hash table is stored in this file. */
   RandomAccessFile dictionaryFile;
